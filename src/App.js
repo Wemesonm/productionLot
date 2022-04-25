c1e276cb9274f00b827a4157b78d56d661f62ac7
Wemeson <wemeson@Wemesons-iMac.local>
2022-04-25 04:14:37 -0400

    add forms

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import {BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './components/page/Home';
 import Company from './components/page/Company';
 import Contact from './components/page/Contact';
--- src/components/page/Home.js
+++ src/components/page/Home.js
@@ -1,13 +1,14 @@
-import style from './Home.module.css';
+import LinkButton from '../layout/LinkButton';
+import styles from './Home.module.css';
 
 
 function Home(){
     
     return (
-    <section>
+    <section className={styles.homeContainer}>
         <h1>Bem Vindo ao <span>Wemeson</span></h1>
         <p>Comece a gerenciar os seus projetos agora mesmo!</p>
-        <a href="/">Criar Projeto</a>
+        <LinkButton to="/newproject" text="Criar Projetos" />
     </section>
     )
 }
